4d66fac3c49ee050a131c007468e11602e60206d
k1863 <57972610+k1863@users.noreply.github.com>
2021-01-25 15:23:18 -0300

    preparation for deployment

--- app.js
+++ app.js
@@ -5,17 +5,10 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
 
-require("dotenv/config");
-
-//middlewares
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const PORT = process.env.PORT || 5000;
 
 //import items route
 const itemsRoute = require("./routes/itemRoute");
-
-const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.DB_CONNECTION, {
   useNewUrlParser: true,
@@ -32,7 +25,12 @@ mongoose.connection.on("error", (err) => {
   console.log("Mongoose Default Connection Error: " + err);
 });
 
-app.use(express.static("client/build"));
+//middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "client/build")));
 app.use("/items", itemsRoute);
 
 //Listening to the server
